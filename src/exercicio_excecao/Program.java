3a3c1555e1561ccab3d63c2f3b656d3108e60e93
Matheus Duarte <matheus.sduarte.ms@gmail.com>
2026-05-27 21:51:05 -0300

    Implemented custom exception handling for withdraw

--- src/exercicio_excecao/Program.java
+++ src/exercicio_excecao/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Account;
+import model.exceptions.DomainException;
 
 public class Program {
 
@@ -12,24 +13,29 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
 
-		Account acc = new Account(number, holder, balance, withdrawLimit);
+			Account acc = new Account(number, holder, balance, withdrawLimit);
 
-		System.out.println();
-		System.out.print("Enter amount for withdraw: ");
-		double amount = sc.nextDouble();
-		acc.withdraw(amount);
-		System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			acc.withdraw(amount);
+			System.out.printf("New balance: %.2f%n", acc.getBalance());
+
+		} catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
 
 		sc.close();
 	}
--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 public class Account {
 
 	private Integer number;
@@ -44,12 +46,22 @@ public class Account {
 	public void setWithdrawLimit(Double withdrawLimit) {
 		this.withdrawLimit = withdrawLimit;
 	}
-	
+
 	public void deposit(double amount) {
+
 		balance += amount;
 	}
 
 	public void withdraw(double amount) {
+
+		if (amount > withdrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+
+		if (amount > balance) {
+			throw new DomainException("Not enough balance");
+		}
+
 		balance -= amount;
 	}
 }
